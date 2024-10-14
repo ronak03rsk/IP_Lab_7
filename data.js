b132c0df3dc3e9e4cd15e4683eb3b09b1c0501aa
ronak03rsk <142477252+ronak03rsk@users.noreply.github.com>
2024-10-14 09:57:08 +0000

    The files have been created successfully

---
+++ data.js
@@ -0,0 +1,42 @@
+const products = [
+    {
+      id: 1,
+      name: 'albany sofa',
+      image:
+        'https://images.pexels.com/photos/462118/pexels-photo-462118.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      price: 39.95,
+      desc: `I'm baby direct trade farm-to-table hell of, YOLO readymade raw denim venmo whatever organic gluten-free kitsch schlitz irony af flexitarian.`,
+    },
+    {
+      id: 2,
+      name: 'entertainment center',
+      image:
+        'https://images.pexels.com/photos/6012907/pexels-photo-6012907.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      price: 29.98,
+      desc: `I'm baby direct trade farm-to-table hell of, YOLO readymade raw denim venmo whatever organic gluten-free kitsch schlitz irony af flexitarian.`,
+    },
+    {
+      id: 3,
+      name: 'albany sectional',
+      image:
+        'https://cdn.pixabay.com/photo/2023/06/14/23/12/sunset-8064078_1280.jpg',
+      price: 10.99,
+      desc: `I'm baby direct trade farm-to-table hell of, YOLO readymade raw denim venmo whatever organic gluten-free kitsch schlitz irony af flexitarian.`,
+    },
+    {
+      id: 4,
+      name: 'leather sofa',
+      image:
+        'https://dl.airtable.com/.attachments/3245c726ee77d73702ba8c3310639727/f000842b/product-5.jpg',
+      price: 9.99,
+      desc: `I'm baby direct trade farm-to-table hell of, YOLO readymade raw denim venmo whatever organic gluten-free kitsch schlitz irony af flexitarian.`,
+    },
+  ]
+  const people = [
+    { id: 1, name: 'john' },
+    { id: 2, name: 'peter' },
+    { id: 3, name: 'susan' },
+    { id: 4, name: 'anna' },
+    { id: 5, name: 'emma' },
+  ]
+  module.exports = { products, people }
